96103123bbaf04ca3fe56bc56d26dd53a8f83ce5
kiljh <kiljh@me.com>
2019-04-02 15:36:11 +0900

    step1-6 1~9dan

--- codesquad1.playground/Contents.swift
+++ codesquad1.playground/Contents.swift
@@ -1,14 +1,23 @@
 import UIKit
 
-var j = 2
-var gugudan = [Int].init(repeating: 0, count: 9)
-
-var index=0
-for i in gugudan {
-    gugudan[index] = j*(index+1)
-    index+=1
+//gugu() 함수 선언부(callee)
+func gugu(number : Int) {
+    var j = number
+    var gugudan = [Int].init(repeating: 0, count: 9)
+    
+    var index=0
+    for i in gugudan {
+        gugudan[index] = j*(index+1)
+        index+=1
+    }
+    
+    for i in gugudan {
+        print("\(j) * \(i/j) = \(i)")
+    }
 }
 
-for i in gugudan {
-    print("\(j) * \(i/j) = \(i)")
+//gugu() 함수 호출부(caller)
+for i in 1...9 {
+    gugu(number: i)
 }
+

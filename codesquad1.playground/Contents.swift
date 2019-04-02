cb05bd1f111c5b8c29ed9359689fab6c443de95a
kiljh <kiljh@me.com>
2019-04-02 14:51:16 +0900

    step1-5 odd

--- codesquad1.playground/Contents.swift
+++ codesquad1.playground/Contents.swift
@@ -2,9 +2,14 @@ import UIKit
 
 
 for j in 2..<10 {
-    print("")
-    print("\(j)단")
-    for i in 1..<10 {
-        print("\(j) * \(i) = \(j*i)")
+    if (j % 2 == 1) {
+        print("")
+        print("\(j)단")
+        for i in 1..<10 {
+            print("\(j) * \(i) = \(j*i)")
+        }
+    } else {
+        // 짝수
     }
+    
 }

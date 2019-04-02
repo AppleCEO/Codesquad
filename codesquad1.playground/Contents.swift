bdd2617c133b955908305c5fe29bc11841466119
kiljh <kiljh@me.com>
2019-04-02 14:49:54 +0900

    step1-5 all

--- codesquad1.playground/Contents.swift
+++ codesquad1.playground/Contents.swift
@@ -1,81 +1,10 @@
 import UIKit
 
-print("2단")
-print("2 * 1 = \(2*1)")
-print("2 * 2 = \(2*2)")
-print("2 * 3 = \(2*3)")
-print("2 * 4 = \(2*4)")
-print("2 * 5 = \(2*5)")
-print("2 * 6 = \(2*6)")
-print("2 * 7 = \(2*7)")
-print("2 * 8 = \(2*8)")
-print("2 * 9 = \(2*9)")
-print("")
-print("3단")
-print("3 * 1 = \(3*1)")
-print("3 * 2 = \(3*2)")
-print("3 * 3 = \(3*3)")
-print("3 * 4 = \(3*4)")
-print("3 * 5 = \(3*5)")
-print("3 * 6 = \(3*6)")
-print("3 * 7 = \(3*7)")
-print("3 * 8 = \(3*8)")
-print("3 * 9 = \(3*9)")
-print("")
-let dan4 = 4
-print("4단")
-var result = dan4 * 1
-print("\(dan4) * 1 = \(result)")
-result = 4 * 2
-print("\(dan4) * 2 = \(result)")
-result = 4 * 3
-print("\(dan4) * 3 = \(result)")
-result = 4 * 4
-print("\(dan4) * 4 = \(result)")
-result = 4 * 5
-print("\(dan4) * 5 = \(result)")
-result = 4 * 6
-print("\(dan4) * 6 = \(result)")
-result = 4 * 7
-print("\(dan4) * 7 = \(result)")
-result = 4 * 8
-print("\(dan4) * 8 = \(result)")
-result = 4 * 9
-print("\(dan4) * 9 = \(result)")
-print("")
-let dan5 = 5
-print("5단")
-result = dan5 * 1
-print("\(dan5) * 1 = \(result)")
-result = 5 * 2
-print("\(dan5) * 2 = \(result)")
-result = 5 * 3
-print("\(dan5) * 3 = \(result)")
-result = 5 * 4
-print("\(dan5) * 4 = \(result)")
-result = 5 * 5
-print("\(dan5) * 5 = \(result)")
-result = 5 * 6
-print("\(dan5) * 6 = \(result)")
-result = 5 * 7
-print("\(dan5) * 7 = \(result)")
-result = 5 * 8
-print("\(dan5) * 8 = \(result)")
-result = 5 * 9
-print("\(dan5) * 9 = \(result)")
-print("")
-let dan6 = 6
-print("6단")
-var i = 1
-while (i < 10) {
-    result = dan6 * i
-    print("\(dan6) * \(i) = \(result)")
-    i = i + 1 //i값을 증가
-}
-print("")
-let dan7 = 7
-print("7단")
-for i in 1..<10 {
-    result = dan7 * i
-    print("\(dan7) * \(i) = \(result)")
+
+for j in 2..<10 {
+    print("")
+    print("\(j)단")
+    for i in 1..<10 {
+        print("\(j) * \(i) = \(j*i)")
+    }
 }

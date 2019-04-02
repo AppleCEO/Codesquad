47b81137aa8a36bfb21762441017440ecb13c5f9
kiljh <kiljh@me.com>
2019-04-02 17:15:31 +0900

    Step2-2 Function

--- UnitConverter/UnitConverter/main.swift
+++ UnitConverter/UnitConverter/main.swift
@@ -13,20 +13,40 @@ let str = readLine()
 // use to round
 let numberOfPlaces = 4.0
 let multiplier = pow(10.0, numberOfPlaces)
+let cmtom:Double = 100
+
+// FUNCTION : convert centimeter To Meter
+func convertCmToM (str: String) -> Void {
+    let doubleValue : Double = NSString(string: str as NSString).doubleValue
+    var result = Double(doubleValue/cmtom)
+    result = round(result * multiplier) / multiplier
+    print ("\(result)m")
+}
+
+// FUNCTION : convert Meter To centimeter
+func convertMToCm (str: String ) -> Void {
+    let doubleValue : Double = NSString(string: str as NSString).doubleValue
+    var result = Double(doubleValue*cmtom)
+    result = round(result * multiplier) / multiplier
+    print ("\(result)cm")
+}
 
 
-if str == nil {
-    
-} else {
-    if str!.suffix(2) == "cm" {
-        let doubleValue : Double = NSString(string: str! as NSString).doubleValue
-        var result = Double(doubleValue/100)
-        result = round(result * multiplier) / multiplier
-        print ("\(result)m")
-    } else { // else if str.suffix(1) == "m"
-        let doubleValue : Double = NSString(string: str! as NSString).doubleValue
-        var result = Double(doubleValue*100)
-        result = round(result * multiplier) / multiplier
-        print ("\(result)cm")
+// FUNCTION : unitConverter
+func unitConverter (input: String) -> Void {
+    if str == nil {
+        
+    } else {
+        if str!.suffix(2) == "cm" {
+            convertCmToM(str: String(str!))
+        } else if str!.suffix(1) == "m" {
+            convertMToCm(str: String(str!))
+        }
     }
+}
+
+
+// RUN
+if let test = str {
+    unitConverter(input: test)
 }

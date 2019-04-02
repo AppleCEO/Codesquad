af0535db2c88cb43a40f358fb776b65a4bd41e13
kiljh <kiljh@me.com>
2019-04-02 14:41:20 +0900

    step1-2 2,3dan

--- codesquad1.playground/Contents.swift
+++ codesquad1.playground/Contents.swift
@@ -1,5 +1,25 @@
 import UIKit
 
-var str = "Hello, iOS World"
+print("2단")
+print("2 * 1 = \(2*1)")
+print("2 * 2 = \(2*2)")
+print("2 * 3 = \(2*3)")
+print("2 * 4 = \(2*4)")
+print("2 * 5 = \(2*5)")
+print("2 * 6 = \(2*6)")
+print("2 * 7 = \(2*7)")
+print("2 * 8 = \(2*8)")
+print("2 * 9 = \(2*9)")
+print("")
+print("3단")
+print("3 * 1 = \(3*1)")
+print("3 * 2 = \(3*2)")
+print("3 * 3 = \(3*3)")
+print("3 * 4 = \(3*4)")
+print("3 * 5 = \(3*5)")
+print("3 * 6 = \(3*6)")
+print("3 * 7 = \(3*7)")
+print("3 * 8 = \(3*8)")
+print("3 * 9 = \(3*9)")
 
-print(str)
+

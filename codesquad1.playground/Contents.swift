7a12ed390e741a9c80f2bfce8662fa3dbc07e776
kiljh <kiljh@me.com>
2019-04-02 14:42:38 +0900

    step1-3 4,5dan

--- codesquad1.playground/Contents.swift
+++ codesquad1.playground/Contents.swift
@@ -21,5 +21,46 @@ print("3 * 6 = \(3*6)")
 print("3 * 7 = \(3*7)")
 print("3 * 8 = \(3*8)")
 print("3 * 9 = \(3*9)")
-
+print("")
+let dan4 = 4
+print("4단")
+var result = dan4 * 1
+print("\(dan4) * 1 = \(result)")
+result = 4 * 2
+print("\(dan4) * 2 = \(result)")
+result = 4 * 3
+print("\(dan4) * 3 = \(result)")
+result = 4 * 4
+print("\(dan4) * 4 = \(result)")
+result = 4 * 5
+print("\(dan4) * 5 = \(result)")
+result = 4 * 6
+print("\(dan4) * 6 = \(result)")
+result = 4 * 7
+print("\(dan4) * 7 = \(result)")
+result = 4 * 8
+print("\(dan4) * 8 = \(result)")
+result = 4 * 9
+print("\(dan4) * 9 = \(result)")
+print("")
+let dan5 = 5
+print("5단")
+result = dan5 * 1
+print("\(dan5) * 1 = \(result)")
+result = 5 * 2
+print("\(dan5) * 2 = \(result)")
+result = 5 * 3
+print("\(dan5) * 3 = \(result)")
+result = 5 * 4
+print("\(dan5) * 4 = \(result)")
+result = 5 * 5
+print("\(dan5) * 5 = \(result)")
+result = 5 * 6
+print("\(dan5) * 6 = \(result)")
+result = 5 * 7
+print("\(dan5) * 7 = \(result)")
+result = 5 * 8
+print("\(dan5) * 8 = \(result)")
+result = 5 * 9
+print("\(dan5) * 9 = \(result)")
 

f11fab452c675774143515c5cab16c454a7dcceb
kiljh <kiljh@me.com>
2019-04-04 18:27:03 +0900

    Step2-5 yard length change

--- UnitConverter/UnitConverter/main.swift
+++ UnitConverter/UnitConverter/main.swift
@@ -11,10 +11,10 @@ import Darwin
 
 let mToCm:Double = 100
 let inchToCm:Double = 2.54
-
+let yardToCm:Double = 91.44
 
 func convert (_ num: Double, _ from: String, _ to: String) -> (Double, String) {
-    var result: (Double, String)
+    var result = (num, to)
     
     switch from {
     case "cm" :
@@ -23,56 +23,87 @@ func convert (_ num: Double, _ from: String, _ to: String) -> (Double, String) {
         result = convertFromM((num, to))
     case "inch" :
         result = convertFromInch((num, to))
+    case "yard":
+        result = convertFromYard((num, to))
     default :
-        print("지원하지 않는 단위입니다")
-        unitConverter()
-        exit(0)
+        unavailableFeet()
     }
+    
     return result
+}
+
+func unavailableFeet() {
+    print("지원하지 않는 단위입니다")
+    unitConverter()
+    exit(0)
 }
 
 func convertFromCm (_ input: (number: Double, to: String) ) -> (Double, String) {
     var result = (input.number, input.to)
     
     switch input.to {
+    case "yard":
+        result.0 = input.number/yardToCm
     case "inch":
         result.0 = input.number/inchToCm
-    default:
+    case "", "m":
         result.0 = input.number/mToCm
         result.1 = "m"
+    default:
+        unavailableFeet()
     }
     
     return result
 }
 
 func convertFromM (_ input: (number: Double, to: String) ) -> (Double, String) {
-    var result: (Double, String)
+    var result = (input.number, input.to)
     
     switch input.to {
+    case "yard":
+        result = convertFromM((input.number, "cm"))
+        result = convertFromCm((result.0, input.to))
     case "inch":
         result = convertFromM((input.number, "cm"))
         result = convertFromCm((result.0, input.to))
-    default:
+    case "", "cm":
         result = (input.number*mToCm, "cm")
+    default:
+        unavailableFeet()
     }
     
     return result
 }
 
 func convertFromInch (_ input: (number: Double, to: String) ) -> (Double, String) {
-    var result: (Double, String)
+    var result = (input.number, input.to)
     
     switch input.to {
     case "m":
         result = convertFromInch((input.number, "cm"))
         result = convertFromCm((result.0, input.to))
-    default:
+    case "","cm":
         result = (input.number*inchToCm, "cm")
+    default:
+        unavailableFeet()
     }
     
     return result
 }
 
+func convertFromYard (_ input: (number: Double, to: String) ) -> (Double, String) {
+    var result = (input.number, input.to)
+    
+    switch input.to {
+    case "","m":
+        result = (input.number*yardToCm, "m")
+        result = convertFromCm(result)
+    default:
+        unavailableFeet()
+    }
+    
+    return result
+}
 
 // 입력받아 숫자와 단위를 구분하는 함수
 func inputsMaker(_ `in` : String)->(Double, String){
@@ -104,15 +135,19 @@ func unitsMaker(_ unit : String)->(String, String){
 func unitConverter() -> Void {
     let inputFromUser = readLine()
     
+    if inputFromUser == "quit" || inputFromUser == "q" {
+        exit(0)
+    }
     if let input = inputFromUser {
-        var (num, unit) = inputsMaker(input)
-        var (from, to) = unitsMaker(unit)
+        let (num, unit) = inputsMaker(input)
+        let (from, to) = unitsMaker(unit)
         
-        var result = convert(num, from, to)
+        let result = convert(num, from, to)
         print("\(result.0)\(result.1)")
+        
+        unitConverter()
     }
 }
-
 
 
 // RUN

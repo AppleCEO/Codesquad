805a20ed53f06b1028842c506eeb0cd0338be208
kiljh <kiljh@me.com>
2019-04-04 11:32:57 +0900

    Step2-5 refectoring
    https://gist.github.com/kiljh-me/811be07b7a4329ad6709491706bf432b

--- UnitConverter/UnitConverter/main.swift
+++ UnitConverter/UnitConverter/main.swift
@@ -6,86 +6,76 @@
 //  Copyright © 2019 hw. All rights reserved.
 //
 import Foundation
+import Darwin
 
-
-// use to round
-let numberOfPlaces = 4.0
-let multiplier = pow(10.0, numberOfPlaces)
-
-// cm to m, m to cm
-let cmTom:Double = 100
+// feet
+let mToCm:Double = 100
 let inchToCm:Double = 2.54
+let yardToCm:Double = 91.44
 
-// FUNCTION : convert centimeter To Meter
-func convertCmToM (str: String) -> String {
-    let doubleValue : Double = NSString(string: str as NSString).doubleValue
-    var result = Double(doubleValue/cmTom)
-    result = round(result * multiplier) / multiplier
-    
-    return "\(result)m"
+
+func convert (_ arr : (doubleValue: Double, feet: String), mode: String) -> (Double, String) {
+    switch mode {
+    case "CmToM" :
+        return (arr.doubleValue/mToCm, "m")
+    case "MToCm" :
+        return (arr.doubleValue*mToCm, "cm")
+    case "CmToInch" :
+        return (arr.doubleValue/inchToCm, "inch")
+    case "InchToCm" :
+        return (arr.doubleValue*inchToCm, "cm")
+    case "CmToYard" :
+        return (arr.doubleValue/yardToCm, "yard")
+    case "YardToCm" :
+        return (arr.doubleValue*yardToCm, "cm")
+    default :
+        print("지원하지 않는 모드입니다")
+        unitConverter()
+        return (arr.doubleValue, "a")
+    }
 }
 
-// FUNCTION : convert Meter To centimeter
-func convertMToCm (str: String ) -> String {
-    let doubleValue : Double = NSString(string: str as NSString).doubleValue
-    var result = Double(doubleValue*cmTom)
-    result = round(result * multiplier) / multiplier
-    
-    return "\(result)cm"
-}
 
-// FUNCTION : convert Inch To Meter
-func convertCmToInch (str: String) -> String {
-    let doubleValue : Double = NSString(string: str as NSString).doubleValue
-    var result = Double(doubleValue/inchToCm)
-    result = round(result * multiplier) / multiplier
-    
-    return "\(result)inch"
-}
-
-// FUNCTION : convert Meter To centimeter
-func convertInchToCm (str: String ) -> String {
-    let doubleValue : Double = NSString(string: str as NSString).doubleValue
-    var result = Double(doubleValue*inchToCm)
-    result = round(result * multiplier) / multiplier
-    
-    return "\(result)cm"
-}
-
-// FUNCTION : unitConverter
 func unitConverter() -> Void {
     // input from user
-    var str = readLine()
+    let str = readLine()
+    
+    let inputNumber : Double = NSString(string: str! as NSString).doubleValue
+    var inputTuple = (0.0, "start")
     
     if str == nil {
         
     } else {
         if str!.suffix(7) == "cm inch" {
-            str = convertCmToInch(str: String(str!))
-            print(str!)
+            inputTuple = convert( (inputNumber, "cm"), mode: "CmToInch")
         } else if str!.suffix(6) == "inch m" {
-            str = convertInchToCm(str: String(str!))
-            str = convertCmToM(str: String(str!))
-            print(str!)
+            inputTuple = convert( (inputNumber, "inch"), mode: "InchToCm")
+            inputTuple = convert(inputTuple, mode: "CmToM")
         } else if str!.suffix(6) == "m inch" {
-            str = convertMToCm(str: String(str!))
-            str = convertCmToInch(str: String(str!))
-            print(str!)
+            inputTuple = convert( (inputNumber, "m"), mode: "MToCm")
+            inputTuple = convert(inputTuple, mode: "CmToInch")
+        } else if str!.suffix(6) == "yard m" || str!.suffix(4) == "yard" {
+            inputTuple = convert((inputNumber, "yard"), mode: "YardToCm")
+            inputTuple = convert(inputTuple, mode: "CmToM")
+        } else if str!.suffix(6) == "m yard" {
+            inputTuple = convert((inputNumber, "m"), mode: "MToCm")
+            inputTuple = convert(inputTuple, mode: "CmToYard")
         } else if str!.suffix(2) == "cm" {
-            str = convertCmToM(str: String(str!))
-            print(str!)
+            inputTuple = convert((inputNumber, "cm"), mode: "CmToM")
         } else if str!.suffix(1) == "m" {
-            str = convertMToCm(str: String(str!))
-            print(str!)
+            inputTuple = convert((inputNumber, "m"), mode: "MToCm")
         } else if str!.suffix(4) == "inch" {
-            str = convertInchToCm(str: String(str!))
-            print(str!)
+            inputTuple = convert((inputNumber, "inch"), mode: "InchToCm")
+        } else if str! == "quit" || str! == "q" {
+            exit(0)
         } else {
             print("지원하지 않는 단위입니다.")
-            unitConverter()
         }
+        print("\(inputTuple.0)\(inputTuple.1)")
+        unitConverter()
     }
 }
+
 
 
 // RUN
